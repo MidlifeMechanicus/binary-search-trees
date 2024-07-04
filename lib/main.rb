585654a76638f08217ddd478f403d0cc912f1cf8
ElectronDragon <electricdragondevelopment@gmail.com>
2024-07-04 08:57:16 +0100

    Write driver script

--- lib/main.rb
+++ lib/main.rb
@@ -1,26 +1,28 @@
 require_relative "main/tree"
 require_relative "main/node"
 
-# array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
-an_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+array = Array.new(15) {rand(1..100)}
 
 tree = Tree.new
-
-p tree.depth
-
-tree.build_tree(an_array)
-
+tree.build_tree(array)
 p tree.balanced?
 
-tree.insert(11)
-tree.insert(12)
-tree.insert(13)
-tree.insert(14)
-tree.insert(15)
+p tree.level_order
+p tree.preorder
+p tree.inorder
+p tree.postorder
+
+new_array = Array.new(5) {rand(101..200)}
+
+new_array.each do |element|
+  tree.insert(element)
+end
 
 p tree.balanced?
-
 tree.rebalance
-
 p tree.balanced?
+
+p tree.level_order
+p tree.preorder
+p tree.inorder
+p tree.postorder
